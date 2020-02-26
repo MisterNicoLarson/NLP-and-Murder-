43b7451dae38f7aa73f0641db559f5ffe636230d
Nicolas <nclpetitjean@gmail.co>
2020-02-26 20:22:31 +0100

    Lire du xml
    Fonction qui permet de recuperer un texte dans une ou plusieurs balises :
    "{http://www.mediawiki.org/xml/export-0.10/}text"

--- lireXML.py
+++ lireXML.py
@@ -3,84 +3,33 @@ from lxml import etree
 from lxml import html
 import xml.etree.ElementTree  as ET 
 
-""" Fonction script"""   
-            
-def arbreXML (pathDataFileXML):
+"""Fonction"""
+
+def recuperationText(path):
     # Entree : string qui est le chemin vers nos données XML
-    # Sortie : liste de string qui est le nom de balise XML
-    tree = etree.parse(pathDataFileXML) # c'est le dossier dans lequel on a du XML
-    root = tree.getroot() # permet de recuperer la racine
-    #Boucle qui va donner a chaque fois le nom des elements dans les balises depuis root
-    etage = [root.tag] # la base de notre arbre va etre sa racine root.tag
-    for leaf in root:
-        etage.append(leaf.tag)
-        for leaf1 in leaf:
-            etage.append(leaf1.tag)
-            for leaf2 in leaf1 :
-                etage.append(leaf2.tag)
-    test = leaf1.find("{http://www.mediawiki.org/xml/export-0.10/}text") # regarder si on peut ou pas recuperer ceci dans une var à la con
-    print(test.text)
-    return etage
-
-def cheminParcoursArbre(liste):
-    # Objectif : obtenir ce chemin /mediawiki/page/revision/text
-    # Entrée : une liste qui est notre arbre
-    # Sortie : un chemin qui est le chemin de notre arbre
-    # Methode : "/".join(liste) qui permet de transformer une liste en string
-    way = "/"+"/".join(liste)
-    return way
-
-def nettoyageListe(liste):
-    # Entree : liste
-    # Sortie : liste nettoyée
-    # Objectif : je reçois une liste de nom de balise et je vais la purger de tout ce qui est inutile
-    listeClean = []
-    listeClean.append(liste[0]) # ici on ajoute notre root
-    page = liste.index("{http://www.mediawiki.org/xml/export-0.10/}page")
-    revision = liste.index("{http://www.mediawiki.org/xml/export-0.10/}revision")
-    text = liste.index("{http://www.mediawiki.org/xml/export-0.10/}text")
-    listeClean.append(liste[page])
-    listeClean.append(liste[revision])
-    listeClean.append(liste[text])
-    return listeClean
-
-def textBaliseText(pathXML, pathArbre):
-    # Entree : prends 2 variables string
-    #           -> pathXML : c'est le chemin vers le dossier XML 
-    #           -> pathXML : "listSerialKiller.xml"
-    #           -> pathArbre : c'est le chemin dans l'arbre pour acceder au texte
-    #           -> pathArbre : /{http://www.mediawiki.org/xml/export-0.10/}mediawiki/{http://www.mediawiki.org/xml/export-0.10/}page/{http://www.mediawiki.org/xml/export-0.10/}revision/{http://www.mediawiki.org/xml/export-0.10/}text
-    # Sortie : le texte dans la balise texte
-    # Objectif : renvoyer le text qui est dans la balise text
-    # Problème : lien hypertext car str ne pose pas de pb mais str hypertext il aime pas
-    tree = etree.parse(pathXML)
-    print("Voici pathArbre : "+ pathArbre)
-    print("Voici le type de pathArbre "+str(type(pathArbre)))
-    newText = tree.findtext(pathArbre)
-    print(newText)
+    # Sortie : l'objet de la balise soit un texte
     
-
-def conversionArbreText (pathDataFileXML):
-    # Entree : string qui est le chemin vers nos données XML
-    # Sortie : l'objet de la balise soir un texte
-    tree = etree.parse(pathDataFileXML) # c'est le dossier dans lequel on a du XML
+    #Etape 1 : l'extraction des balise textes
+    tree = etree.parse(path) # c'est le dossier dans lequel on a du XML
     root = tree.getroot() # permet de recuperer la racine
-    #Boucle qui va donner a chaque fois le nom des elements dans les balises depuis root
-    etage = [root.tag] # la base de notre arbre va etre sa racine root.tag
-    for leaf in root:
-        etage.append(leaf.tag)
-        for leaf1 in leaf:
-            etage.append(leaf1.tag)
-            for leaf2 in leaf1 :
-                etage.append(leaf2.tag)
-    text = leaf1.find("{http://www.mediawiki.org/xml/export-0.10/}text")
-    # je regarde dans leaf1 si j'ai une balise text
-    # je stock mon text dans une var
-    return text.text
+    childrenRoot = root.getchildren()
+    liste = []
+    for rdc in childrenRoot:
+        for etage in rdc:
+            #print(elt1.getchildren())
+            captureText = etage.findall("{http://www.mediawiki.org/xml/export-0.10/}text")# captureText est une liste
+            for textBalise in captureText:
+                liste.append(textBalise.text)
+    
+    # Etape 2 : transformer la liste de texte en super texte
+    megaText = " ".join(liste)
+    
+    #Renvoie le texte
+    return megaText
 
 """Traitement de l'information""" 
 
-pathFile = "listSerialKiller.xml"
-listXML = arbreXML(pathFile)
-texte = conversionArbreText(pathFile)
+pathFile = "essai.xml"
+path = "listSerialKiller.xml"
+texte = recuperationText(path)
 print(texte)
