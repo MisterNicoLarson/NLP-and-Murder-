41391d75c755536321c51a5b5e2096892da9d095
Nicolas <nclpetitjean@gmail.co>
2020-04-06 13:59:36 +0200

    resolution pb de path

--- Python/ScriptExecution.py
+++ Python/ScriptExecution.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from ScriptFonction import *
+from essaiTkinter import *
 
 pathFile = "essai.xml"
 path = "listSerialKiller.xml"
+pathTest = recupererFichier()
 texte = recuperationText(path)
 print(texte)
